1c6eebfb770b4ff8d07748dfc7de9956bc86c1b8
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-05-05 21:49:01 +0300

    added "patient" & "appointment" list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DoctorListComponent } from './features/doctor/doctor-list/doctor-list.component';
+import { PatientListComponent } from './features/patient/patient-list/patient-list.component';
+import { AppointmentListComponent } from './features/appointment/appointment-list/appointment-list.component';
 
 const routes: Routes = [
   {
     path: 'admin/doctors',
     component: DoctorListComponent
+  },
+  {
+    path: 'admin/patients',
+    component: PatientListComponent
+  },
+  {
+    path: 'admin/appointments',
+    component: AppointmentListComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { DoctorListComponent } from './features/doctor/doctor-list/doctor-list.component';
+import { PatientListComponent } from './features/patient/patient-list/patient-list.component';
+import { AppointmentListComponent } from './features/appointment/appointment-list/appointment-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    DoctorListComponent
+    DoctorListComponent,
+    PatientListComponent,
+    AppointmentListComponent
   ],
   imports: [
     BrowserModule,
