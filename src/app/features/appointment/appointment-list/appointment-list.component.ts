2b0e5a950b6c897fe5070fe68b0b67d1346f188a
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-05-11 02:00:13 +0300

    added functionality for "Appointment" comp

--- src/app/features/appointment/appointment-list/appointment-list.component.ts
+++ src/app/features/appointment/appointment-list/appointment-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppointmentService } from '../services/appointment.service';
+import { Appointment } from '../models/appointment.model';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
+  appointments?: Appointment[];
 
+  constructor(private appointmentService: AppointmentService) {
+  }
+  ngOnInit(): void {
+    this.appointmentService.getAllAppointments()
+    .subscribe({
+      next: (response) => {
+        this.appointments = response;
+      }
+    });
+  }
+
+  
 }
